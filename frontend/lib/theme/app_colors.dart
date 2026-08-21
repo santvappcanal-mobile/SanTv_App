4b85ce6850b66fdca5957fb6594fed5ee2fec230
andreslandines <cardosoandres331@gmail.com>
2026-08-20 22:01:46 -0500

    actualizando colores de la pagina principal

--- frontend/lib/theme/app_colors.dart
+++ frontend/lib/theme/app_colors.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const background = Color(0xFF121212);
-  static const cardDark = Color(0xFF1E1E1E);
+  static const background = Color(0xFF0D0D0D);
+  static const cardDark = Color.fromARGB(255, 21, 21, 21);
   static const green = Color(0xFF00E676);
   static const greyText = Color(0xFFB0B0B0);
+
+  static const backgroundGradient = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    colors: [
+      Color(0xFF2A2A2A),
+      Color(0xFF0A0A0A),
+    ],
+  );
 }
